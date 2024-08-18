d8c7cb12ee284ea33c3939653e7bfd74a98223e8
RassilonX <chrisfraser18@gmail.com>
2024-08-18 09:04:45 +0100

    Solved first test case

--- StringAdderConsoleApp/StringAdderLibrary/StringCalculator.cs
+++ StringAdderConsoleApp/StringAdderLibrary/StringCalculator.cs
@@ -6,6 +6,9 @@ public class StringCalculator : IStringCalculator
 {
     public int Calculate(string stringToCalculate)
     {
+        if (String.IsNullOrEmpty(stringToCalculate)) 
+            return 0;
+
         return 0;
     }
 }
