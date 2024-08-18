e3a53dc0a05ea4561e401420c915ca415e15dff9
RassilonX <chrisfraser18@gmail.com>
2024-08-18 09:02:08 +0100

    Added all test cases present in kata

--- StringAdderConsoleApp/StringAdderLibraryTests/StringCalculatorTests.cs
+++ StringAdderConsoleApp/StringAdderLibraryTests/StringCalculatorTests.cs
@@ -1,8 +1,10 @@
+using StringCalculatorLibrary;
+
 namespace StringCalculatorLibraryTests;
 
 public class StringCalculatorTests
 {
-    public static IEnumerable<object[]> StringCalculatorConditions =>
+    public static IEnumerable<object[]> StringCalculatorPositiveCases =>
     new[]
     {
         new object[] { "", 0 },
@@ -19,15 +21,37 @@ public class StringCalculatorTests
     };
 
     [Theory]
-    [MemberData(nameof(StringCalculatorConditions))]
-    public void Calculate_PositiveIntegersReturnsCorrectTotal(string stringToCalculate, int result)
+    [MemberData(nameof(StringCalculatorPositiveCases))]
+    public void Calculate_PositiveIntegersReturnsCorrectTotal(string stringToCalculate, int expectedResult)
     {
+        // Arrange
+        var stringCalculator = new StringCalculator();
 
+        // Act
+        var result = stringCalculator.Calculate(stringToCalculate);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+        Assert.IsType<int>(result);
     }
 
-    [Fact]
-    public void Calculate_NegativeNumbersThrowsException()
+    public static IEnumerable<object[]> StringCalculatorNegativeCases =>
+    new[]
     {
+        new object[] { "-1,2", "Negatives not allowed: -1" },
+        new object[] { "2,-4,3,-5", "Negatives not allowed: -4,-5" },
+        // Add more test data here as appropriate
+    };
 
+    [Theory]
+    [MemberData(nameof(StringCalculatorNegativeCases))]
+    public void Calculate_NegativeNumbersThrowsException(string stringToCalculate, string exceptionMessage)
+    {
+        // Arrange
+        var stringCalculator = new StringCalculator();
+
+        // Act - Assert
+        var ex = Assert.Throws<Exception>(() => stringCalculator.Calculate(stringToCalculate));
+        Assert.Equal(exceptionMessage, ex.Message);
     }
 }
