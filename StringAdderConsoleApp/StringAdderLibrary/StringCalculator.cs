159029cbc41a0bf159393f1cbe0eefc10c5c7654
RassilonX <chrisfraser18@gmail.com>
2024-08-18 09:28:04 +0100

    Solved for negative numbers

--- StringAdderConsoleApp/StringAdderLibrary/StringCalculator.cs
+++ StringAdderConsoleApp/StringAdderLibrary/StringCalculator.cs
@@ -10,7 +10,7 @@ public class StringCalculator : IStringCalculator
         if (string.IsNullOrEmpty(stringToCalculate)) 
             return 0;
 
-        string[] numbers = Regex.Split(stringToCalculate, @"\D+");
+        string[] numbers = Regex.Split(stringToCalculate, @"[^\d-]+");
 
         List<int> result = new List<int>();
         foreach (string number in numbers)
@@ -18,9 +18,17 @@ public class StringCalculator : IStringCalculator
             if (!string.IsNullOrEmpty(number))
             {
                 int integer = int.Parse(number);
+
                 if (integer < 1001)
                     result.Add(integer);
             }
+        }
+
+        // Check for negative numbers
+        List<int> negativeNumbers = result.FindAll(i => i < 0);
+        if (negativeNumbers.Count > 0)
+        {
+            throw new Exception($"Negatives not allowed: {string.Join(",", negativeNumbers)}");
         }
 
         return result.Sum();
